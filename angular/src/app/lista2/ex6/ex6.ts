a93ff8b44b5f8b55350aeba2d13e6192d19a141a
NogariKauaa <273554@unifio.edu.br>
2026-09-04 01:15:24 +0000

    Resolvendo exercicio 6

--- angular/src/app/lista2/ex6/ex6.ts
+++ angular/src/app/lista2/ex6/ex6.ts
@@ -4,6 +4,18 @@ import { Component } from '@angular/core';
   selector: 'app-ex6',
   standalone: false,
   templateUrl: './ex6.html',
-  styleUrl: './ex6.css',
+  styleUrl: './ex6.css'
 })
-export class Ex6 {}
+export class Ex6 {
+  quantidade = 0;
+
+  aumentar() {
+    this.quantidade = this.quantidade + 1;
+  }
+
+  diminuir() {
+    if (this.quantidade > 0) {
+      this.quantidade = this.quantidade - 1;
+    }
+  }
+}
