fd70474a0b942e2671fd860d0cd67bafc94ca109
NogariKauaa <273554@unifio.edu.br>
2026-09-04 01:23:13 +0000

    Resolvendo desafio final

--- angular/src/app/lista2/desafio/desafio.ts
+++ angular/src/app/lista2/desafio/desafio.ts
@@ -4,6 +4,24 @@ import { Component } from '@angular/core';
   selector: 'app-desafio',
   standalone: false,
   templateUrl: './desafio.html',
-  styleUrl: './desafio.css',
+  styleUrl: './desafio.css'
 })
-export class Desafio {}
+export class Desafio {
+  nomeAluno = '';
+  quantidadeDisciplinas = 1;
+  mensagem = '';
+
+  aumentarDisciplinas() {
+    this.quantidadeDisciplinas = this.quantidadeDisciplinas + 1;
+  }
+
+  diminuirDisciplinas() {
+    if (this.quantidadeDisciplinas > 1) {
+      this.quantidadeDisciplinas = this.quantidadeDisciplinas - 1;
+    }
+  }
+
+  realizarMatricula() {
+    this.mensagem = 'Aluno ' + this.nomeAluno + ' matriculado em ' + this.quantidadeDisciplinas + ' disciplina(s).';
+  }
+}
