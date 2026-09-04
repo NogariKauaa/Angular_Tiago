1e1f95d8311227c2813c26d7bb228898fa266d3d
NogariKauaa <273554@unifio.edu.br>
2026-09-04 01:20:34 +0000

    Resolvendo exercicio 11

--- angular/src/app/lista2/ex11/ex11.ts
+++ angular/src/app/lista2/ex11/ex11.ts
@@ -4,6 +4,25 @@ import { Component } from '@angular/core';
   selector: 'app-ex11',
   standalone: false,
   templateUrl: './ex11.html',
-  styleUrl: './ex11.css',
+  styleUrl: './ex11.css'
 })
-export class Ex11 {}
+export class Ex11 {
+  produto = 'Mouse Gamer';
+  preco = 120;
+  quantidade = 1;
+  mensagem = '';
+
+  aumentar() {
+    this.quantidade = this.quantidade + 1;
+  }
+
+  diminuir() {
+    if (this.quantidade > 1) {
+      this.quantidade = this.quantidade - 1;
+    }
+  }
+
+  adicionarAoCarrinho() {
+    this.mensagem = this.quantidade + ' x ' + this.produto + ' adicionado ao carrinho!';
+  }
+}
